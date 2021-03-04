3458e967a1d1c296937c73882c9a08de19775645
Yaroslav Yakovlev <ekb-buza@mail.ru>
2021-03-04 18:45:25 +0500

    разбор на вторую часть

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 
+void PartOne(std::string strAddr){
+    std::string strPatrOne;
+    strPatrOne = strAddr.substr(0, strAddr.find("@"));
+    std::cout << "strPatrOne: " << strPatrOne << std::endl;
+}
+
+void PartTwo(std::string strAddr){
+    std::string strPartTwo;
+    strPartTwo = strAddr.substr((strAddr.find("@") + 1), strAddr.length());
+    std::cout << "strPartTwo: " << strPartTwo << std::endl;
+}
+
 int main()
 {
     std::string strAddres;
     std::string strPatrOne;
+    std::string strPartTwo;
 	std::cout << "Emai addresses" << std::endl;
     std::cout << "Enter your email address" << std::endl;
     std::cin >> strAddres;
     std::cout << "strAddres: " << strAddres << std::endl;
-    strPatrOne = strAddres.substr(0, strAddres.find("@"));   
-    std::cout << "strPatrOne: " << strPatrOne << std::endl;
+    PartOne(strAddres);
+    PartTwo(strAddres);
     return 0;
 }
