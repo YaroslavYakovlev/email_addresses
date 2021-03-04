87a02cb16343e5e6598b35bea26cfecf93695aa1
Yaroslav Yakovlev <ekb-buza@mail.ru>
2021-03-04 18:21:06 +0500

    разбор на первую часть

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
-using namespace std;
-
-
 int main()
 {
-	cout << "Emai addresses" << endl;
+    std::string strAddres;
+    std::string strPatrOne;
+	std::cout << "Emai addresses" << std::endl;
+    std::cout << "Enter your email address" << std::endl;
+    std::cin >> strAddres;
+    std::cout << "strAddres: " << strAddres << std::endl;
+    strPatrOne = strAddres.substr(0, strAddres.find("@"));   
+    std::cout << "strPatrOne: " << strPatrOne << std::endl;
     return 0;
 }
